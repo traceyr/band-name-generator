ea1800fbd396aa5d18188b27f5e9d5e9a18f9524
Tracey <radcliffe.tracey@gmail.com>
2015-06-18 19:15:47 -0700

    commiting all of my work

--- app/script.js
+++ app/script.js
@@ -1,18 +1,37 @@
 $(document).ready(function() {
 
+  $('button').on('click', function () {
 
-$('button').on('click', function () {
-	$.get('http://localhost:3000/adjective', function(response){
-		var adjective = response.word;
-		$('#adjective').text(adjective);
-	});
-  $.get('http://localhost:3000/verb', function(response){
-    var verb = response.word;
-    $('#verb').text(verb);
-  })
-  $.get('http://localhost:3000/noun', function(response){
-    var noun = response.word;
-    $('#noun').text(noun);
-  })
-});
+  	$.get('http://localhost:3000/adjective', function(response){
+  		var adjective = response.word;
+  		$('#adjective').text(adjective);
+  	});
+
+    $.get('http://localhost:3000/verb', function(response){
+      var verb = response.word;
+      $('#verb').text(verb);
+    })
+
+    $.get('http://localhost:3000/noun', function(response){
+      var noun = response.word;
+      $('#noun').text(noun);
+    })
+  });
+
+  $('#submitWords').on('submit', function(e) {
+      e.preventDefault();
+
+      var adjective = $("input[name=adjective]").val();
+
+
+    var adjPost;
+
+    if(adjective) {
+      adjPost = {word: adjective};
+      $.post("adjective", adjPost, function(response){
+        var adjectiveRes = response.msg;
+        $("#adjectiveRes").text(adjectiveRes);
+      });
+    }
+  });
 });
--- server.js
+++ server.js
@@ -1,21 +1,28 @@
 'use strict';
 
 var express = require('express');
+var bodyparser = require("body-parser");
+// var Adjective = require('./lib/adjectives');
+// var getRandomWord = require('./lib/getRandomWord')
 var app = express();
 var port = process.env.PORT || 3000;
+
+app.use(bodyparser.json());
+app.use(bodyparser.urlencoded({extended: true}));
 
 app.use(express.static(__dirname + '/app/'));
 
 var Adjective = function() {
-	this.weary = true;
-	this.vengeful = true;
-	this.sophisticated = true;
-	this.yellowish = true;
-	this.bogus = true;
-}
+  this.weary = true;
+  this.vengeful = true;
+  this.sophisticated = true;
+  this.yellowish = true;
+  this.bogus = true;
+ };
 
 var adjective = new Adjective();
 
+//move verb and noun objects to new seperate viles and creat vars and requre them in
 var Verb = function(){
 this.awake = true;
 this.beaten = true;
@@ -32,14 +39,35 @@ var Noun = function(){
 	this.vein = true;
 	this.linen = true;
 	this.creature = true;
+	this.rain = true;
+	this.battleStar = true;
 }
 
 var noun = new Noun();
 
 function getRandomWord(object) {
-	var propArray = Object.keys(object);
-	var randomProp = propArray[Math.floor(Math.random() * propArray.length)];
-	return {word: randomProp};
+  var propArray = Object.keys(object);
+  var randomProp = propArray[Math.floor(Math.random() * propArray.length)];
+  return {word: randomProp};
+};
+
+//move postRandomWords to another file and create a var require
+
+function postRandomWord (word, object) {
+	//if the word doesnt exist
+	//if the word does exist
+
+	//check if the word exists
+	if (object.hasOwnProperty(word)) {
+		return{msg: "This word is already in existense within our generator. Please try again."}
+	} else {
+		object[word] = true;
+		return {msg: "Thanks for the new word " + word + ". It has been added to the generator"}
+	}
+
+	//if the word doesnt exist, add it as a property to that object
+	//and send msg back thanking for word
+	//if the word does exist, send msg back
 }
 
 app.get('/', function(req, res) {
@@ -56,6 +84,11 @@ app.get('/verb', function(req, res) {
 
 app.get('/noun', function(req, res) {
 	res.json(getRandomWord(noun));
+});
+
+app.post('/adjective', function(req, res) {
+	var word = postRandomWord(req.body.word, adjective);
+	res.json(word);
 });
 
 app.listen(port, function() {

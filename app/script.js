86a5791339739502647d35ddaa9467e8d0c7d80c
Tracey <radcliffe.tracey@gmail.com>
2015-06-26 20:25:14 -0700

    finished project after grunt

--- app/script.js
+++ app/script.js
@@ -1,70 +1,69 @@
+'use strict';
+
 $(document).ready(function() {
 
   $('button').on('click', function() {
 
-  	$.get("adjective", function(response){
-  		var adjective = response.word;
-  		$('#adjective').text(adjective);
-  	});
+    $.get('adjective', function(response) {
+      var adjective = response.word;
+      $('#adjective').text(adjective);
+    });
 
-    $.get("verb", function(response){
+    $.get('verb', function(response) {
       var verb = response.word;
       $('#verb').text(verb);
-    })
+    });
 
-    $.get("noun", function(response){
+    $.get('noun', function(response) {
       var noun = response.word;
       $('#noun').text(noun);
-    })
+    });
   });
 
   $('#submitWords').on('submit', function(e) {
-      e.preventDefault();
+    e.preventDefault();
 
-      var adjective = $("input[name=adjective]").val();
-
+    var adjective = $('input[name=adjective]').val();
 
     var adjPost;
 
-    if(adjective) {
+    if (adjective) {
       adjPost = {word: adjective};
-      $.post("adjective", adjPost, function(response){
+      $.post('adjective', adjPost, function(response) {
         var adjectiveRes = response.msg;
-        $("#adjectiveRes").text(adjectiveRes);
+        $('#adjectiveRes').text(adjectiveRes);
       });
     }
   });
 
-$('#submitNoun').on('submit', function(e) {
-      e.preventDefault();
+  $('#submitNoun').on('submit', function(e) {
+    e.preventDefault();
 
-      var noun = $("input[name=noun").val();
-
+    var noun = $('input[name=noun').val();
 
     var nounPost;
 
-    if(noun) {
+    if (noun) {
       nounPost = {word: noun};
-      $.post("noun", nounPost, function(response){
+      $.post('noun', nounPost, function(response) {
         var nounRes = response.msg;
-        $("#nounRes").text(nounRes);
+        $('#nounRes').text(nounRes);
       });
     }
   });
 
-$('#submitVerb').on('submit', function(e) {
-      e.preventDefault();
+  $('#submitVerb').on('submit', function(e) {
+    e.preventDefault();
 
-      var verb = $("input[name=verb]").val();
-
+    var verb = $('input[name=verb]').val();
 
     var verbPost;
 
-    if(verb) {
+    if (verb) {
       verbPost = {word: verb};
-      $.post("verb", verbPost, function(response){
+      $.post('verb', verbPost, function(response) {
         var verbRes = response.msg;
-        $("#verbRes").text(verbRes);
+        $('#verbRes').text(verbRes);
       });
     }
   });
--- server.js
+++ server.js
@@ -1,7 +1,7 @@
 'use strict';
 
 var express = require('express');
-var bodyparser = require("body-parser");
+var bodyparser = require('body-parser');
 var Adjective = require('./lib/adjectives.js');
 var Noun = require('./lib/noun.js');
 var Verb = require('./lib/verb.js');
@@ -22,36 +22,36 @@ var verb = new Verb();
 var noun = new Noun();
 
 app.get('/', function(req, res) {
-	res.sendFile('index.html');
+  res.sendFile('index.html');
 });
 
 app.get('/adjective', function(req, res) {
-	res.json(getRandomWord(adjective));
+  res.json(getRandomWord(adjective));
 });
 
 app.get('/verb', function(req, res) {
-	res.json(getRandomWord(verb));
+  res.json(getRandomWord(verb));
 });
 
 app.get('/noun', function(req, res) {
-	res.json(getRandomWord(noun));
+  res.json(getRandomWord(noun));
 });
 
 app.post('/adjective', function(req, res) {
-	var word = postRandomWord(req.body.word, adjective);
-	res.json(word);
+  var word = postRandomWord(req.body.word, adjective);
+  res.json(word);
 });
 
 app.post('/noun', function(req, res) {
-	var word = postRandomWord(req.body.word, noun);
-	res.json(word);
+  var word = postRandomWord(req.body.word, noun);
+  res.json(word);
 });
 
 app.post('/verb', function(req, res) {
-	var word = postRandomWord(req.body.word, verb);
-	res.json(word);
+  var word = postRandomWord(req.body.word, verb);
+  res.json(word);
 });
 
 app.listen(port, function() {
-	console.log("server available at http://localhost:" + port);
+  console.log('server available at http://localhost:' + port);
 });
